073c5342bd92a64482e406d6457a64d2efaa87e2
弭忧萌虾 <2211479860@qq.com>
2025-01-18 05:41:22 +0800

    Initial commit

---
+++ .gradle/8.8/dependencies-accessors/0143f8cf0c72a75d74cfaabd9d9cfc8e099d0c43/sources/org/gradle/accessors/dm/LibrariesForLibs.java
@@ -0,0 +1,363 @@
+package org.gradle.accessors.dm;
+
+import org.gradle.api.NonNullApi;
+import org.gradle.api.artifacts.MinimalExternalModuleDependency;
+import org.gradle.plugin.use.PluginDependency;
+import org.gradle.api.artifacts.ExternalModuleDependencyBundle;
+import org.gradle.api.artifacts.MutableVersionConstraint;
+import org.gradle.api.provider.Provider;
+import org.gradle.api.model.ObjectFactory;
+import org.gradle.api.provider.ProviderFactory;
+import org.gradle.api.internal.catalog.AbstractExternalDependencyFactory;
+import org.gradle.api.internal.catalog.DefaultVersionCatalog;
+import java.util.Map;
+import org.gradle.api.internal.attributes.ImmutableAttributesFactory;
+import org.gradle.api.internal.artifacts.dsl.CapabilityNotationParser;
+import javax.inject.Inject;
+
+/**
+ * A catalog of dependencies accessible via the {@code libs} extension.
+ */
+@NonNullApi
+public class LibrariesForLibs extends AbstractExternalDependencyFactory {
+
+    private final AbstractExternalDependencyFactory owner = this;
+    private final VersionAccessors vaccForVersionAccessors = new VersionAccessors(providers, config);
+    private final BundleAccessors baccForBundleAccessors = new BundleAccessors(objects, providers, config, attributesFactory, capabilityNotationParser);
+    private final PluginAccessors paccForPluginAccessors = new PluginAccessors(providers, config);
+
+    @Inject
+    public LibrariesForLibs(DefaultVersionCatalog config, ProviderFactory providers, ObjectFactory objects, ImmutableAttributesFactory attributesFactory, CapabilityNotationParser capabilityNotationParser) {
+        super(config, providers, objects, attributesFactory, capabilityNotationParser);
+    }
+
+    /**
+     * Dependency provider for <b>anvilcraft</b> with <b>dev.dubhe:anvilcraft-neoforge-1.21.1</b> coordinates and
+     * with version reference <b>anvilcraft</b>
+     * <p>
+     * This dependency was declared in catalog libs.versions.toml
+     */
+    public Provider<MinimalExternalModuleDependency> getAnvilcraft() {
+        return create("anvilcraft");
+    }
+
+    /**
+     * Dependency provider for <b>anvillib</b> with <b>dev.anvilcraft.lib:anvillib-neoforge-1.21.1</b> coordinates and
+     * with version reference <b>anvillib</b>
+     * <p>
+     * This dependency was declared in catalog libs.versions.toml
+     */
+    public Provider<MinimalExternalModuleDependency> getAnvillib() {
+        return create("anvillib");
+    }
+
+    /**
+     * Dependency provider for <b>clothConfig</b> with <b>me.shedaniel.cloth:cloth-config-neoforge</b> coordinates and
+     * with version reference <b>clothConfig</b>
+     * <p>
+     * This dependency was declared in catalog libs.versions.toml
+     */
+    public Provider<MinimalExternalModuleDependency> getClothConfig() {
+        return create("clothConfig");
+    }
+
+    /**
+     * Dependency provider for <b>curios</b> with <b>top.theillusivec4.curios:curios-neoforge</b> coordinates and
+     * with version reference <b>curios</b>
+     * <p>
+     * This dependency was declared in catalog libs.versions.toml
+     */
+    public Provider<MinimalExternalModuleDependency> getCurios() {
+        return create("curios");
+    }
+
+    /**
+     * Dependency provider for <b>emi</b> with <b>dev.emi:emi-neoforge</b> coordinates and
+     * with version reference <b>emi</b>
+     * <p>
+     * This dependency was declared in catalog libs.versions.toml
+     */
+    public Provider<MinimalExternalModuleDependency> getEmi() {
+        return create("emi");
+    }
+
+    /**
+     * Dependency provider for <b>jade</b> with <b>maven.modrinth:jade</b> coordinates and
+     * with version reference <b>jade</b>
+     * <p>
+     * This dependency was declared in catalog libs.versions.toml
+     */
+    public Provider<MinimalExternalModuleDependency> getJade() {
+        return create("jade");
+    }
+
+    /**
+     * Dependency provider for <b>jeiCommonApi</b> with <b>mezz.jei:jei-1.21.1-common-api</b> coordinates and
+     * with version reference <b>jei</b>
+     * <p>
+     * This dependency was declared in catalog libs.versions.toml
+     */
+    public Provider<MinimalExternalModuleDependency> getJeiCommonApi() {
+        return create("jeiCommonApi");
+    }
+
+    /**
+     * Dependency provider for <b>jeiForgeApi</b> with <b>mezz.jei:jei-1.21.1-neoforge-api</b> coordinates and
+     * with version reference <b>jei</b>
+     * <p>
+     * This dependency was declared in catalog libs.versions.toml
+     */
+    public Provider<MinimalExternalModuleDependency> getJeiForgeApi() {
+        return create("jeiForgeApi");
+    }
+
+    /**
+     * Dependency provider for <b>jeiForgeImpl</b> with <b>mezz.jei:jei-1.21.1-neoforge</b> coordinates and
+     * with version reference <b>jei</b>
+     * <p>
+     * This dependency was declared in catalog libs.versions.toml
+     */
+    public Provider<MinimalExternalModuleDependency> getJeiForgeImpl() {
+        return create("jeiForgeImpl");
+    }
+
+    /**
+     * Dependency provider for <b>kubejs</b> with <b>dev.latvian.mods:kubejs-neoforge</b> coordinates and
+     * with version reference <b>kubejs</b>
+     * <p>
+     * This dependency was declared in catalog libs.versions.toml
+     */
+    public Provider<MinimalExternalModuleDependency> getKubejs() {
+        return create("kubejs");
+    }
+
+    /**
+     * Dependency provider for <b>patchouli</b> with <b>vazkii.patchouli:Patchouli</b> coordinates and
+     * with version reference <b>patchouli</b>
+     * <p>
+     * This dependency was declared in catalog libs.versions.toml
+     */
+    public Provider<MinimalExternalModuleDependency> getPatchouli() {
+        return create("patchouli");
+    }
+
+    /**
+     * Dependency provider for <b>registrate</b> with <b>com.tterrag.registrate:Registrate</b> coordinates and
+     * with version reference <b>registrate</b>
+     * <p>
+     * This dependency was declared in catalog libs.versions.toml
+     */
+    public Provider<MinimalExternalModuleDependency> getRegistrate() {
+        return create("registrate");
+    }
+
+    /**
+     * Dependency provider for <b>reiApi</b> with <b>me.shedaniel:RoughlyEnoughItems-api-neoforge</b> coordinates and
+     * with version reference <b>rei</b>
+     * <p>
+     * This dependency was declared in catalog libs.versions.toml
+     */
+    public Provider<MinimalExternalModuleDependency> getReiApi() {
+        return create("reiApi");
+    }
+
+    /**
+     * Dependency provider for <b>reiForge</b> with <b>me.shedaniel:RoughlyEnoughItems-neoforge</b> coordinates and
+     * with version reference <b>rei</b>
+     * <p>
+     * This dependency was declared in catalog libs.versions.toml
+     */
+    public Provider<MinimalExternalModuleDependency> getReiForge() {
+        return create("reiForge");
+    }
+
+    /**
+     * Dependency provider for <b>reiPlugin</b> with <b>me.shedaniel:RoughlyEnoughItems-default-plugin-neoforge</b> coordinates and
+     * with version reference <b>rei</b>
+     * <p>
+     * This dependency was declared in catalog libs.versions.toml
+     */
+    public Provider<MinimalExternalModuleDependency> getReiPlugin() {
+        return create("reiPlugin");
+    }
+
+    /**
+     * Dependency provider for <b>theoneprobe</b> with <b>mcjty.theoneprobe:theoneprobe</b> coordinates and
+     * with version reference <b>theoneprobe</b>
+     * <p>
+     * This dependency was declared in catalog libs.versions.toml
+     */
+    public Provider<MinimalExternalModuleDependency> getTheoneprobe() {
+        return create("theoneprobe");
+    }
+
+    /**
+     * Group of versions at <b>versions</b>
+     */
+    public VersionAccessors getVersions() {
+        return vaccForVersionAccessors;
+    }
+
+    /**
+     * Group of bundles at <b>bundles</b>
+     */
+    public BundleAccessors getBundles() {
+        return baccForBundleAccessors;
+    }
+
+    /**
+     * Group of plugins at <b>plugins</b>
+     */
+    public PluginAccessors getPlugins() {
+        return paccForPluginAccessors;
+    }
+
+    public static class VersionAccessors extends VersionFactory  {
+
+        public VersionAccessors(ProviderFactory providers, DefaultVersionCatalog config) { super(providers, config); }
+
+        /**
+         * Version alias <b>anvilcraft</b> with value <b>1.4.2+build.897</b>
+         * <p>
+         * If the version is a rich version and cannot be represented as a
+         * single version string, an empty string is returned.
+         * <p>
+         * This version was declared in catalog libs.versions.toml
+         */
+        public Provider<String> getAnvilcraft() { return getVersion("anvilcraft"); }
+
+        /**
+         * Version alias <b>anvillib</b> with value <b>1.1.0+build.97</b>
+         * <p>
+         * If the version is a rich version and cannot be represented as a
+         * single version string, an empty string is returned.
+         * <p>
+         * This version was declared in catalog libs.versions.toml
+         */
+        public Provider<String> getAnvillib() { return getVersion("anvillib"); }
+
+        /**
+         * Version alias <b>clothConfig</b> with value <b>15.0.130</b>
+         * <p>
+         * If the version is a rich version and cannot be represented as a
+         * single version string, an empty string is returned.
+         * <p>
+         * This version was declared in catalog libs.versions.toml
+         */
+        public Provider<String> getClothConfig() { return getVersion("clothConfig"); }
+
+        /**
+         * Version alias <b>curios</b> with value <b>9.0.15+1.21.1</b>
+         * <p>
+         * If the version is a rich version and cannot be represented as a
+         * single version string, an empty string is returned.
+         * <p>
+         * This version was declared in catalog libs.versions.toml
+         */
+        public Provider<String> getCurios() { return getVersion("curios"); }
+
+        /**
+         * Version alias <b>emi</b> with value <b>1.1.13+1.21.1</b>
+         * <p>
+         * If the version is a rich version and cannot be represented as a
+         * single version string, an empty string is returned.
+         * <p>
+         * This version was declared in catalog libs.versions.toml
+         */
+        public Provider<String> getEmi() { return getVersion("emi"); }
+
+        /**
+         * Version alias <b>jade</b> with value <b>15.3.4+neoforge</b>
+         * <p>
+         * If the version is a rich version and cannot be represented as a
+         * single version string, an empty string is returned.
+         * <p>
+         * This version was declared in catalog libs.versions.toml
+         */
+        public Provider<String> getJade() { return getVersion("jade"); }
+
+        /**
+         * Version alias <b>jei</b> with value <b>19.17.0.193</b>
+         * <p>
+         * If the version is a rich version and cannot be represented as a
+         * single version string, an empty string is returned.
+         * <p>
+         * This version was declared in catalog libs.versions.toml
+         */
+        public Provider<String> getJei() { return getVersion("jei"); }
+
+        /**
+         * Version alias <b>kubejs</b> with value <b>2101.7.1-build.181</b>
+         * <p>
+         * If the version is a rich version and cannot be represented as a
+         * single version string, an empty string is returned.
+         * <p>
+         * This version was declared in catalog libs.versions.toml
+         */
+        public Provider<String> getKubejs() { return getVersion("kubejs"); }
+
+        /**
+         * Version alias <b>modDevGradle</b> with value <b>2.0.34-beta</b>
+         * <p>
+         * If the version is a rich version and cannot be represented as a
+         * single version string, an empty string is returned.
+         * <p>
+         * This version was declared in catalog libs.versions.toml
+         */
+        public Provider<String> getModDevGradle() { return getVersion("modDevGradle"); }
+
+        /**
+         * Version alias <b>patchouli</b> with value <b>1.21-87-NEOFORGE</b>
+         * <p>
+         * If the version is a rich version and cannot be represented as a
+         * single version string, an empty string is returned.
+         * <p>
+         * This version was declared in catalog libs.versions.toml
+         */
+        public Provider<String> getPatchouli() { return getVersion("patchouli"); }
+
+        /**
+         * Version alias <b>registrate</b> with value <b>MC1.21-1.3.0+55</b>
+         * <p>
+         * If the version is a rich version and cannot be represented as a
+         * single version string, an empty string is returned.
+         * <p>
+         * This version was declared in catalog libs.versions.toml
+         */
+        public Provider<String> getRegistrate() { return getVersion("registrate"); }
+
+        /**
+         * Version alias <b>rei</b> with value <b>16.0.762</b>
+         * <p>
+         * If the version is a rich version and cannot be represented as a
+         * single version string, an empty string is returned.
+         * <p>
+         * This version was declared in catalog libs.versions.toml
+         */
+        public Provider<String> getRei() { return getVersion("rei"); }
+
+        /**
+         * Version alias <b>theoneprobe</b> with value <b>1.21_neo-12.0.3-5</b>
+         * <p>
+         * If the version is a rich version and cannot be represented as a
+         * single version string, an empty string is returned.
+         * <p>
+         * This version was declared in catalog libs.versions.toml
+         */
+        public Provider<String> getTheoneprobe() { return getVersion("theoneprobe"); }
+
+    }
+
+    public static class BundleAccessors extends BundleFactory {
+
+        public BundleAccessors(ObjectFactory objects, ProviderFactory providers, DefaultVersionCatalog config, ImmutableAttributesFactory attributesFactory, CapabilityNotationParser capabilityNotationParser) { super(objects, providers, config, attributesFactory, capabilityNotationParser); }
+
+    }
+
+    public static class PluginAccessors extends PluginFactory {
+
+        public PluginAccessors(ProviderFactory providers, DefaultVersionCatalog config) { super(providers, config); }
+
+    }
+
+}
