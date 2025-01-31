a9aa1f2c07a6ac705efe3dca305e25b58accc3fa
moelobster <2211479860@qq.com>
2025-01-31 16:14:14 +0800

    格式修改

--- src/main/java/moe/lobster/anvilcraft_fooding/init/ModBlocks.java
+++ src/main/java/moe/lobster/anvilcraft_fooding/init/ModBlocks.java
@@ -1,13 +1,22 @@
 package moe.lobster.anvilcraft_fooding.init;
 
+import com.tterrag.registrate.providers.RegistrateRecipeProvider;
 import com.tterrag.registrate.util.entry.BlockEntry;
 import moe.lobster.anvilcraft_fooding.block.ChiliBlock;
 import net.minecraft.advancements.critereon.StatePropertiesPredicate;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.data.recipes.RecipeCategory;
 import net.minecraft.data.recipes.ShapelessRecipeBuilder;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.material.MapColor;
 import net.minecraft.world.level.material.PushReaction;
+import net.minecraft.world.level.storage.loot.LootPool;
+import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.entries.LootItem;
+import net.minecraft.world.level.storage.loot.functions.ApplyBonusCount;
 import net.minecraft.world.level.storage.loot.predicates.LootItemBlockStatePropertyCondition;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
 
@@ -25,21 +34,36 @@ public class ModBlocks {
             .pushReaction(PushReaction.DESTROY))
         .blockstate((context, provider) -> {
         })
-        .loot((table, block) -> {
+        .loot((tables, block) -> {
+            HolderLookup.RegistryLookup<Enchantment> lookup = tables.getRegistries().lookupOrThrow(Registries.ENCHANTMENT);
             LootItemCondition.Builder builder = LootItemBlockStatePropertyCondition.hasBlockStateProperties(ModBlocks.CHILI_CROP.get())
                 .setProperties(StatePropertiesPredicate.Builder.properties().hasProperty(ChiliBlock.AGE, 7));
-            table.add(block, table.createCropDrops(ModBlocks.CHILI_CROP.get(), ModItems.CHILI.get(), ModBlocks.CHILI_CROP.asItem(), builder));
+            tables.add(
+                ModBlocks.CHILI_CROP.get(),
+                tables.applyExplosionDecay(
+                    ModBlocks.CHILI_CROP.get(),
+                    LootTable.lootTable()
+                        .withPool(LootPool.lootPool().add(LootItem.lootTableItem(ModBlocks.CHILI_CROP.asItem())))
+                        .withPool(
+                            LootPool.lootPool()
+                                .when(builder)
+                                .add(
+                                    LootItem.lootTableItem(ModItems.CHILI.get())
+                                        .apply(ApplyBonusCount.addBonusBinomialDistributionCount(lookup.getOrThrow(Enchantments.FORTUNE), 0.5714286F, 3))
+                                )
+                        )
+                )
+            );
         })
         .item()
         .model((context, provider) -> {
         })
         .tag()
-        .recipe((ctx, provider) -> {
+        .recipe((ctx, provider) ->
             ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ctx.get())
                 .requires(ModItems.CHILI.get())
-                .unlockedBy("has_chili", provider.has(ModItems.CHILI.get()))
-                .save(provider);
-        })
+                .unlockedBy("has_chili", RegistrateRecipeProvider.has(ModItems.CHILI.get()))
+                .save(provider))
         .build()
         .register();
 
